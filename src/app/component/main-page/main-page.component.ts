eef080908877ee6b075a40f8002a3ed0bb306830
root <dbs.aymen@gmail.com>
2020-01-13 13:13:44 +0100

    add date Constructor

--- src/app/component/main-page/main-page.component.ts
+++ src/app/component/main-page/main-page.component.ts
@@ -17,12 +17,42 @@ export class MainPageComponent implements OnInit {
   constructor(private evenementService: EvenementService, private publicationService: PublicationService) {
 
   }
+  dateConstructor(day,month,year,dayAdd,monthAdd,yearAdd){
+    let newDay=day;
+    let newMonth=month;
+    let newYear=year;
+    newDay+=dayAdd;
+    if(newDay>30){
+      newMonth++;
+      newDay=newDay-30;
+    }
+    if(newDay<1){
+      newDay=30+newDay;
+      newMonth--;
+    }
+    newMonth+=monthAdd;
+    if(newMonth>12){
+      newYear++;
+      newMonth=newMonth-12;
+    }
+    if(newMonth<1){
+      newYear--;
+      newMonth=12+newMonth;
+    }
+    newYear+=yearAdd;
+    return(newDay.toString()+'-'+newMonth.toString()+'-'+newYear.toString());
+  }
 
   ngOnInit() {
 
     let date = new Date();
-    let startDate = (date.getDate() - 1).toString() + "-" + (date.getMonth() + 1).toString() + "-" + date.getFullYear().toString();
-    let endDate = (date.getDate() + 1).toString() + "-" + (date.getMonth() + 1).toString() + "-" + (date.getFullYear() + 1).toString();
+    let day=date.getDate();
+    let month=date.getMonth()+1;
+    let year=date.getFullYear();
+
+    let startDate=this.dateConstructor(day,month,year,1,0,0);
+    let endDate=this.dateConstructor(day,month,year,0,0,1);
+
     this.evenementService.findByDateEvtBetween(startDate, endDate).subscribe(
       (data: Evenement[]) => {
         this.events = data;
@@ -31,10 +61,9 @@ export class MainPageComponent implements OnInit {
         console.log("Error while loading Events");
       }
     );
-    startDate = (date.getDate() + 1).toString() + "-" + date.getMonth().toString() + "-" + date.getFullYear().toString();
-    endDate = date.getDate().toString() + "-" + (date.getMonth() - 1).toString() + "-" + date.getFullYear().toString();
-
-    this.publicationService.findByDateBetween(startDate, endDate).subscribe((data: Publication[]) => {
+    startDate=this.dateConstructor(day,month,year,1,0,0);
+    endDate=this.dateConstructor(day,month,year,0,-1,0);
+    this.publicationService.findByDateBetween(endDate, startDate).subscribe((data: Publication[]) => {
         this.pubs = data;
       },
       err => {
